0f9e274ecc96c8bbfd6805b3057397b7db61cce6
sanjaMijalkovic <sanja.mijalkovic@yahoo.com>
2015-01-08 14:32:45 +0100

    Izmenjena pretraga, dodati neki elementi validacije, formiraj .json za slanje serveru

--- client/pretraga.php
+++ client/pretraga.php
@@ -12,56 +12,60 @@
         {{ tr.oznaka }} **: <input type="text" name="oznaka" ng-maxlength="15" ng-model="oznaka" ng-required='true'/>  
         <span ng-show='formPretraga.oznaka.$error.maxlength'> {{ tr.oznaka_error_length }}</span><br/> 
         {{ tr.natpis }}: <input type="text" name="natpis" ng-model="natpis"/> 
-        <select name="natpisArg" ng-model="natpisArguments">
-            <option value="prazno" ng-value="false"> </option>
-            <option value="and" ng-value="true"> AND </option>
-            <option value="or" ng-value="true"> OR </option>
-            <option value="andNot" ng-value="true"> AND NOT </option>
+        <select name="natpisArg" ng-model="natpisArg" >
+            <option value="prazno" ng-value="prazno"> </option>
+            <option value="and" ng-value="and"> AND </option>
+            <option value="or" ng-value="or"> OR </option>
+            <option value="andNot" ng-value="andNot"> AND NOT </option>
         </select>
-        <input type="text" name="natpis2" ng-show="natpisArguments" /> <br/>
-        <input type="checkbox" name="rezimIgnorisanjaZagrada" value="rezimIgnorisanjaZagrada"> {{ tr.ignorisi_zagrade }}
+        <input type="text" name="natpis2" ng-model="natpis2" ng-show="natpisArg!='prazno'" /> <br/>
+        <input type="checkbox" ng-model="rezimIgnorisanjaZagrada" name="rezimIgnorisanjaZagrada" value="rezimIgnorisanjaZagrada"> {{ tr.ignorisi_zagrade }}
 
     </fieldset>
     <fieldset>
         <legend> {{ tr.izvorno_mesto_nastanka }} </legend>
         {{tr.provincija}}:
-        <select name="provincijaNalaska"  >
+        <select name="provincijaNalaska" ng-model="provincijaNalaska" >
             <option ng-repeat="provincija in provincije | orderBy:'naziv':false"> {{ provincija.naziv }} </option>
         </select><br/>
         {{tr.grad}}: <input type="text" name="gradNalaska" ng-model="gradNalaska"/> <br/>
         {{tr.mesto}}: <input type="text" name="mestoNalaska" ng-model="mestoNalaska"/> <br/>
-		<input type="checkbox" name="prikaziNelokalizovanePodatke" value="prikaziNelokalizovanePodatke"> {{ tr.prikazi_nelokalizovane_podatke}} <br/>
+		<input type="checkbox" name="prikaziNelokalizovanePodatke" ng-model="prikaziNelokalizovanePodatke" value="prikaziNelokalizovanePodatke"> {{ tr.prikazi_nelokalizovane_podatke}} <br/>
     </fieldset>
 	{{tr.moderno_ime_drzave}}:
-        <select name="modernoImeDrzave" >
+        <select name="modernoImeDrzave" ng-model="modernoImeDrzave">
             <option ng-repeat="drzava in drzave | orderBy:'naziv':false"> {{drzava.naziv}} </option>
         </select>
 		<br/>
 		{{tr.moderno_mesto}}
-		<select name="modernoMesto" >
+		<select name="modernoMesto" ng-model="modernoMesto">
             <option ng-repeat="mesto in mesta | orderBy:'naziv':false"> {{mesto.naziv}} </option>
         </select> //TODO <br/>
 		{{tr.pleme}}
-		<select name="pleme" >
+		<select name="pleme" ng-model="pleme">
             <option ng-repeat="p in plemena | orderBy:'naziv':false"> {{p.naziv}} </option>
         </select> //TODO
     <fieldset>
         <legend> {{tr.vreme}} </legend>
-        {{tr.vek}}: <input type="text" name="vek" ng-model="vek"/>
-        <br/>
-        <input type="radio" name="periodVeka" value="prvaPolovinaVeka"/> {{tr.prva_polovina}}  <br/>
-        <input type="radio" name="periodVeka" value="drugaPolovinaVeka"/>  {{tr.druga_polovina}}
-        <br/>
-        <input type="checkbox" name="" value="prikaziNedatovaneNatpise"> {{tr.prikazi_nedatovane_natpise}}
+        {{tr.vek}}: <input type="text" name="vek" ng-model="vek" ng-pattern="/[0-9]+/"/>
+        <span ng-show='formPretraga.vek.$error.pattern'>
+                {{tr.pattern_error_cifre}}
+        </span>		<br/>
+		<div ng-show="!formPretraga.vek.$error.pattern && vek!=null">
+        <input type="radio" name="periodVeka" ng-model="periodVeka" value="prvaPolovinaVeka"/> {{tr.prva_polovina}}  <br/>
+        <input type="radio" name="periodVeka" ng-model="periodVeka" value="drugaPolovinaVeka"/>  {{tr.druga_polovina}}
+        </div>
+		
+        <input type="checkbox" name="" value="prikaziNedatovaneNatpise" ng-model="prikaziNedatovaneNatpise"> {{tr.prikazi_nedatovane_natpise}}
         <br/>
     </fieldset>
     <fieldset>
         <legend> {{tr.sortiraj_rezultate_po}} </legend>
-        <input type="radio" name="sortiranje" value="poVremenu"/> {{tr.vremenu}} <br/>
-        <input type="radio" name="sortiranje" value="poMestuNalaska"/> {{tr.mestu_nalaska}} <br/>
-        <input type="radio" name="sortiranje" value="poVrstiNatpisa"/> {{tr.vrsti_natpisa}} <br/>
+        <input type="radio" name="sortiranje" ng-model="sortiranje" value="poVremenu"  ng-checked="true"/> {{tr.vremenu}} <br/>
+        <input type="radio" name="sortiranje" ng-model="sortiranje" value="poMestuNalaska" /> {{tr.mestu_nalaska}} <br/>
+        <input type="radio" name="sortiranje" ng-model="sortiranje" value="poVrstiNatpisa" /> {{tr.vrsti_natpisa}} <br/>
     </fieldset>
-    <input type="submit" value={{tr.zapocni_pretragu}} ng-click="posaljiPodatke()" ng-disabled='!formPretraga.$valid'  />
+    <input type="submit" value={{tr.zapocni_pretragu}} ng-click="posalji_podatke()" ng-disabled='!formPretraga.$valid'  />
     <input type="reset" value={{tr.resetuj_podatke}} />
 </form>
 <div id="rezultati">
--- client/static/scripts/pretraga.js
+++ client/static/scripts/pretraga.js
@@ -1,8 +1,22 @@
 angular.module('epigrafikaModul').controller('pretragaController', ['$scope', '$http', function ($scope, $http){
-    $scope.oznaka='';
-    $scope.modernoImeDrzave="";
-    $scope.provincijaNalaska="";
-    $scope.natpisArguments=false;
+    $scope.oznaka=null;
+	$scope.natpis=null;
+	$scope.natpis2=null;
+	$scope.natpisArg='prazno';
+	$scope.rezimIgnorisanjaZagrada=false;
+	$scope.provincijaNalaska=null;
+	$scope.gradNalaska=null;
+	$scope.mestoNalaska=null;
+	$scope.prikaziNelokalizovanePodatke=false;
+	$scope.modernoImeDrzave=null;
+	$scope.modernoMesto=null;
+	$scope.pleme=null;
+    $scope.vek=null;
+	$scope.periodVeka=null;
+	$scope.prikaziNedatovaneNatpise=false;
+	$scope.sortiranje='poVremenu';
+	
+    $scope.natpisArgumenti=[" ", "AND", "OR", "AND NOT" ];
     $scope.provincije= null;
     $scope.drzave=null;
 	$scope.plemena=null;
@@ -24,5 +38,63 @@ angular.module('epigrafikaModul').controller('pretragaController', ['$scope', '$
     error(function(data, status, headers, config){
 
     });
+	
+	$http.get('../server/pleme.php', {responseType: 'JSON'}).
+    success(function(data, status, headers, config){
+        if(data!=="null")
+            $scope.plemena=data.data;
+    }).
+    error(function(data, status, headers, config){
+
+    });
+	
+	$http.get('../server/modernoMesto.php', {responseType: 'JSON'}).
+    success(function(data, status, headers, config){
+        if(data!=="null")
+            $scope.mesta=data.data;
+    }).
+    error(function(data, status, headers, config){
+
+    });
+	
+	$scope.posalji_podatke=function(){
+	
+	    if($scope.natpisArg == 'prazno')
+		    $scope.natpis2=null;
+		
+	    if(	$scope.vek=="")
+			$scope.vek=null;
+		if(	$scope.vek==null)
+			$scope.periodVeka=null;
+	    var formData = {
+		    oznaka : $scope.oznaka,
+		    natpis : $scope.natpis,
+		    natpisArg : $scope.natpisArg,
+		    natpis2 : $scope.natpis2,
+		    rezimIgnorisanjaZagrada : $scope.rezimIgnorisanjaZagrada,
+		    provincijaNalaska : $scope.provincijaNalaska,
+		    gradNalaska : $scope.gradNalaska,
+		    mestoNalaska : $scope.mestoNalaska,
+		    prikaziNelokalizovanePodatke : $scope.prikaziNelokalizovanePodatke,
+		    modernoImeDrzave : $scope.modernoImeDrzave,
+		    modernoMesto : $scope.modernoMesto,
+		    pleme: $scope.pleme,
+		    vek : $scope.vek,
+		    periodVeka: $scope.periodVeka,
+		    prikaziNedatovaneNatpise : $scope.prikaziNedatovaneNatpise,
+		    sortiranje: $scope.sortiranje
+	 }
+	var jsonData = angular.toJson(formData);
+	alert(jsonData);
+	 
+	$http.get('../server/objekat.php?podaci='+jsonData, {responseType: 'JSON'}).
+        success(function(data, status, headers, config){
+            if(data!=="null")
+                $scope.mesta=data.data;
+        }).
+        error(function(data, status, headers, config){
+
+        });
+	}
 }]);
 console.info("Inicijalizovan pretragaController.");
