caca12907304047cd095e49474a46bf3e3d35b3e
sanjaMijalkovic <sanja.mijalkovic@yahoo.com>
2015-01-17 14:38:39 +0100

    Dodatna pocetna strana

---
+++ client/pocetna.php
@@ -0,0 +1,15 @@
+<?php include 'header.php'; ?>
+
+<!-- ucitavanje kontrolera -->
+
+<div class="container text-center" style="margin: 100px; " ng-cloak>
+	<div class="row">
+		<div class="col-sm-12">
+			<h1>  {{ tr.dobrodosli }} 
+                        <a href="registracija.php" class=" btn-link">{{tr.ovde}}</a>
+                        </h1>
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.php'; ?>
